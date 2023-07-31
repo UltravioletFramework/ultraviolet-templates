f01069d6df6f9b14f297763020bfb4d031ba90d1
Robert Campbell <waprave@gmail.com>
2023-07-31 15:04:01 -0400

    Fixes to upf template

--- templates/upfgame/UpfGame.Android/Properties/AssemblyInfo.cs
+++ templates/upfgame/UpfGame.Android/Properties/AssemblyInfo.cs
@@ -7,11 +7,11 @@ using Android.App;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("UvGame.Android")]
+[assembly: AssemblyTitle("UpfGame.Android")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("UvGame.Android")]
+[assembly: AssemblyProduct("UpfGame.Android")]
 [assembly: AssemblyCopyright("Copyright ©  2018")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
